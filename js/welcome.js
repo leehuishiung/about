af29f074c046048e2b2c51ad7e74cedba37faa05
leehuishiung <leehuishiung@gmail.com>
2016-12-19 00:28:37 +0800

    Upload js

---
+++ js/welcome.js
@@ -0,0 +1,5 @@
+var style_bold = "font-weight: 700";
+var style_info = "color: #f00"
+var style_normal = "";
+console.log("Hi! I'm %cLEE HUI SHIUNG%c.\n%cWelcome to my personal website %c:-D", style_bold, style_normal, style_bold, style_normal);
+console.log("%cContact me\n%cPhone 1: %c(+86) 156 7631 0192 %c(China)\n%cPhone 2: %c(+60) 18-940 4946 %c(Malaysia)\n%cEmail 1: %cleehuishiung@163.com %c(Recommended)\n%cEmail 2: %cleehuishiung@gmail.com", "color: #088; font-size: 1.2rem; font-weight: 700", style_bold, style_info, "color: #00F", style_bold, style_info, "color: #00F", style_bold, style_info, "color: #00F", style_bold, style_info);
